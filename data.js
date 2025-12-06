d0b8759080ed4e074f1abfb6f2ed7ab511fd7a23
angelawu1023 <angelawu6212@gmail.com>
2025-12-06 21:20:18 +0800

    data.js

---
+++ data.js
@@ -0,0 +1,153 @@
+let Australia = [
+    {
+        "name":"Family",
+        "count":25.44
+    },
+    {
+        "name":"Premium",
+        "count":26.02
+    },
+    {
+        "name":"Student",
+        "count":24.66
+    },
+    {
+        "name":"Free",
+        "count":23.89
+    }
+];
+
+let Germany = [
+    {
+        "name":"Family",
+        "count":25.32
+    },
+    {
+        "name":"Premium",
+        "count":24.83
+    },
+    {
+        "name":"Student",
+        "count":25.32
+    },
+    { 
+        "name":"Free",
+        "count":24.53
+    }
+
+];
+
+let United_States = [
+    {
+        "name":"Family",
+        "count":23.54
+    },
+    {
+        "name":"Premium",
+        "count":27.52
+    },
+    {
+        "name":"Student",
+        "count":24.22
+    },
+    {
+        "name":"Free",
+        "count":24.71
+    },
+];
+
+let India = [
+    {
+        "name":"Family",
+        "count":21.76
+    },
+    {
+        "name":"Premium",
+        "count":28.19
+    },
+    {
+        "name":"Student",
+        "count":24.83
+    },
+    {
+        "name":"Free",
+        "count":25.22
+    },
+];
+
+let France = [
+    {
+        "name":"Family",
+        "count":23.36
+    },
+    {
+        "name":"Premium",
+        "count":27.7
+    },
+    {
+        "name":"Student",
+        "count":23.46
+    },
+    {
+        "name":"Free",
+        "count":25.48
+    },
+];
+
+let United_kingdom = [
+    {
+        "name":"Family",
+        "count":21.43
+    },
+    {
+        "name":"Premium",
+        "count":29.19
+    },
+    {
+        "name":"Student",
+        "count":23.6
+    },
+    {
+        "name":"Free",
+        "count":25.78
+    },
+];
+
+let Canada = [
+    {
+        "name":"Family",
+        "count":24.63
+    },
+    {
+        "name":"Premium",
+        "count":24.53
+    },
+    {
+        "name":"Student",
+        "count":25.05
+    },
+    {
+        "name":"Free",
+        "count":25.79
+    },
+];
+
+let Pakistan = [
+    {
+        "name":"Family",
+        "count":25.23
+    },
+    {
+        "name":"Premium",
+        "count":23.52
+    },
+    {
+        "name":"Student",
+        "count":24.72
+    },
+    {
+        "name":"Free",
+        "count":26.53
+    },
+];
+
